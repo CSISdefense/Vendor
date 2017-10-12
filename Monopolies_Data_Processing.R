f1e8f03733e64b63d69a050f949f513ca11f9c76
Greg Sanders <gsanders@zo>
2017-10-12 15:35:56 -0400

    Updates to monopoly import.

--- Monopolies_Data_Processing.R
+++ Monopolies_Data_Processing.R
@@ -11,11 +11,11 @@ Path<-"C:\\Users\\gsand_000.ALPHONSE\\Documents\\Development\\R-scripts-and-data
 source(paste(Path,"lookups.r",sep=""))
 
 
-file<-unz("Data\\Defense_Vendor_AllVendorHistoryNaics.zip",
-          filename="Defense_Vendor_AllVendorHistoryNaics.txt")
-defense_naics_vendor <- read_tsv(file,
-                          col_names = TRUE,
-                          na = c("","NA","NULL"))
+file<-unz("Data\\Defense_Vendor_EntityIDhistoryNAICS.zip",
+          filename="Defense_Vendor_EntityIDhistoryNAICS.txt")
+# defense_naics_vendor <- read_tsv(file,
+#                           col_names = TRUE,
+#                           na = c("","NA","NULL"))
 
 defense_naics_vendor <- read.table(file,
                            header = TRUE,
@@ -24,15 +24,27 @@ defense_naics_vendor <- read.table(file,
                            sep = "\t")
 
 defense_naics_vendor<-apply_lookups(Path,defense_naics_vendor)
-save(defense_naics_vendor,file="defense_naics_vendor.Rdata")
 
 
-#After some experimentation, I found that grouping
-#By AllContractor, which has over 400k entries, takes long
-#enough that it's better 
+file<-unz("Data\\Defense_Vendor_EntityIDhistory.zip",
+          filename="Defense_Vendor_EntityIDhistory.txt")
+
+defense_vendor <- read.table(file,
+                                   header = TRUE,
+                                   na.strings = c("","NA","NULL"),
+                                   quote="\"",#Necessary because there are some 's in the names.
+                                   sep = "\t")
+
+defense_vendor<-apply_lookups(Path,defense_vendor)
 
 
-names<-unique(defense_naics_vendor$AllContractor)
+
+save(defense_naics_vendor,defense_vendor,file="defense_naics_vendor.Rdata")
+
+
+
+
+names<-unique(defense_vendor$ParentID)
 
 
 names[toupper(names) %in% toupper(c(
@@ -49,34 +61,7 @@ names[toupper(names) %in% toupper(c(
 
 # Filter defense_naics_vendor to only include fical year ranging from 2008 to 2016
 
-defense_naics_vendor_2016 <- filter(defense_naics_vendor,Fiscal.Year >= as.Date("2016-01-01"))
-
-
-defense_vendor_2016<-ddply(defense_naics_vendor_2016,
-                      .(Fiscal.Year,
-                        AllContractor
-                        # ContractorDisplayName,
-                        # jointventure,
-                        # UnknownCompany,
-                        # WarningFlag,
-                        # Small
-                      ),
-                      summarize,
-                      Action.Obligation=sum(Action.Obligation),
-                      numberOfActions=sum(numberOfActions),
-                      Obligation.2016=sum(Obligation.2016)
-)
-
-
-defense_vendor<-ddply(defense_naics_vendor_2016,
-                           .(Fiscal.Year,
-                             AllContractor
-                           ),
-                           mutate,
-                           Action.Obligation=sum(Action.Obligation),
-                           numberOfActions=sum(numberOfActions),
-                           Obligation.2016=sum(Obligation.2016)
-)
+defense_vendor_2016 <- filter(defense_vendor,Fiscal.Year >= as.Date("2016-01-01"))
 
 defense_vendor_2016<-ddply(defense_vendor_2016,
                            .(Fiscal.Year),
@@ -85,7 +70,7 @@ defense_vendor_2016<-ddply(defense_vendor_2016,
                                                ties.method ="min"))
 
 key_vendor_2016<-subset(defense_vendor_2016,pos<=10 |
-         toupper(AllContractor) %in% toupper(c("MDA",  #Check
+         toupper(ParentID) %in% toupper(c("MDA",  #Check
        "DigitalGlobe", #Check
        "United Tech", #Check
        "Rockwell Collins",  #Check
@@ -99,17 +84,17 @@ updated_vendor<-key_vendor_2016
 key_vendor_2016$Sample<-"Original"
 updated_vendor$Sample<-"Merged"
 updated_vendor<-rbind(key_vendor_2016,updated_vendor)
-updated_vendor$VendorName<-updated_vendor$AllContractor
-updated_vendor$VendorName[toupper(updated_vendor$AllContractor)==
+updated_vendor$VendorName<-updated_vendor$ParentID
+updated_vendor$VendorName[toupper(updated_vendor$ParentID)==
                             toupper("DigitalGlobe")&
                             updated_vendor$Sample=="Merged"]<-"MDA"
-updated_vendor$VendorName[toupper(updated_vendor$AllContractor)==
+updated_vendor$VendorName[toupper(updated_vendor$ParentID)==
                             toupper("Rockwell Collins")&
                             updated_vendor$Sample=="Merged"]<-"UNITED TECH"
-updated_vendor$VendorName[toupper(updated_vendor$AllContractor)==
+updated_vendor$VendorName[toupper(updated_vendor$ParentID)==
                             toupper("Orbital Sciences")&
                             updated_vendor$Sample=="Merged"]<-"NORTHROP GRUMMAN"
-updated_vendor$VendorName[toupper(updated_vendor$AllContractor)==
+updated_vendor$VendorName[toupper(updated_vendor$ParentID)==
                             toupper("Aurora Flight Sciences")&
                             updated_vendor$Sample=="Merged"]<-"BOEING"
 
@@ -123,15 +108,20 @@ updated_vendor<-ddply(updated_vendor,
                       Merged.2016 = sum(Obligation.2016)
 )
 
+updated_vendor$ParentLabel<-as.character(updated_vendor$ParentID)
+updated_vendor$ParentLabel[updated_vendor$Sample=="Merged"]<-""
 
 
 
-ggplot(updated_vendor,
+
+ggplot(subset(updated_vendor,pos<=10),
        aes(x=Sample,
            y=Merged.2016,
-           color=AllContractor,
-           group=AllContractor))+
-  geom_line()
+           color=ParentID,
+           group=ParentID))+
+  geom_line()+
+  geom_text(aes(label=ParentLabel))+
+  theme(legend.position="none")
 
 # Transform some columns of interest into factor or integer and deflating
 
