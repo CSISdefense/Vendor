aa24531fee3ce5a648c30e5c0f0d7c5469947e1f
Greg Sanders <gsanders@zo>
2017-10-19 17:08:18 -0600

    Restoring everything after accidentally dropping everything.

---
+++ Monopolies_Data_Processing.R
@@ -0,0 +1,113 @@
+#=================================================================================================================#
+# Data processing for Monopolies Data
+#=================================================================================================================#
+rm(list = ls())
+library(tidyverse)
+library(csis360)
+
+  
+# Path<-"C:\\Users\\gsand_000.ALPHONSE\\Documents\\Development\\R-scripts-and-data\\"
+Path<-"K:\\2007-01 PROFESSIONAL SERVICES\\R scripts and data\\"
+source(paste(Path,"lookups.r",sep=""))
+
+
+file<-unz("Data\\Defense_Vendor_EntityIDhistoryNAICS.zip",
+          filename="Defense_Vendor_EntityIDhistoryNAICS.txt")
+# defense_naics_vendor <- read_tsv(file,
+#                           col_names = TRUE,
+#                           na = c("","NA","NULL"))
+
+#Import Defense vendor list by NAICS.
+defense_naics_vendor <- read.table(file,
+                           header = TRUE,
+                           na.strings = c("","NA","NULL"),
+                           quote="\"",#Necessary because there are some 's in the names.
+                           sep = "\t")
+
+defense_naics_vendor<-apply_lookups(Path,defense_naics_vendor)
+
+#Import Defense Vendor list.
+file<-unz("Data\\Defense_Vendor_EntityIDhistory.zip",
+          filename="Defense_Vendor_EntityIDhistory.txt")
+
+defense_vendor <- read.table(file,
+                                   header = TRUE,
+                                   na.strings = c("","NA","NULL"),
+                                   quote="\"",#Necessary because there are some 's in the names.
+                                   sep = "\t")
+
+defense_vendor<-apply_lookups(Path,defense_vendor)
+
+
+defense_vendor<-csis360::read_and_join(defense_vendor,
+                                       "Lookup_ParentID.csv",
+                                       path="https://raw.githubusercontent.com/CSISdefense/Lookup-Tables/master/",
+                                       by="ParentID",
+                                       directory="vendor//",
+                                       new_var_checked=FALSE,
+                                       add_var="Abbreviation"
+                                       # NA.check.columns="Fair.Competed"
+)
+
+unique(defense_vendor$Abbreviation)
+
+
+
+defense_vendor<-ddply(defense_vendor,
+                      .(Fiscal.Year),
+                      transform, 
+                      pos = rank(-Action.Obligation,
+                                 ties.method ="min"))
+
+
+save(defense_naics_vendor,defense_vendor,file="data//defense_naics_vendor.Rdata")
+
+
+
+
+
+
+
+
+
+
+
+# # Transform some columns of interest into factor or integer and deflating
+# 
+# # sequestration<-deflate(sequestration,
+# #   money_var = "PrimeOrSubObligatedAmount",
+# #   deflator_var="Deflator.2016"
+# # )
+# 
+# 
+# 
+# # Aggregating sequestration by Fiscal.Year, Platform Portfolio and IsSubContract
+# 
+#   #This suddenly stopped working after R / package updates. ??
+# sequestration_Facet<- sequestration %>% dplyr::group_by(Fiscal.Year,
+#                                                   PlatformPortfolio,
+#                                                 #  IsSubContract,
+#                                                   SubCustomer.sum,
+#                                                 SubCustomer.platform,
+#                                                 Pricing.Mechanism.sum,
+#                                                 Pricing.Mechanism.Fee,
+#                                                 ProductServiceOrRnDarea,
+#                                                 ProductServiceOrRnDarea.sum,
+#                                                   Faceting,
+#                                                 Vendor.Size.sum,
+#                                                 Shiny.VendorSize,
+#                                                 IsFSRSreportable) %>%
+#     dplyr::summarise(PrimeOrSubTotalAmount.2016 = sum(PrimeOrSubObligatedAmount.2016)/1e+9)
+# 
+# # Rename 'IsSubContract' column
+# #sequestration_Facet$IsSubContract <- ifelse(sequestration_Facet$IsSubContract == 1,
+# #                                            "SubContract", 
+# #                                            "Prime Contract")
+# # colnames(sequestration_Facet)[3] <- "SubCustomer.sum"
+# 
+# full_data<- sequestration_Facet
+# labels_and_colors<-prepare_labels_and_colors(full_data)
+# 
+# column_key<-csis360::get_column_key(full_data)
+# 
+# save(labels_and_colors,column_key,full_data,file="Shiny Apps//SubContracts//subcontract_full_data.RData")
---
+++ VendorSQL/Vendor_VendorHistoryNaicsPlatformSubCustomer.sql
@@ -0,0 +1,123 @@
+	USE [DIIG]
+GO
+
+/****** Object:  View [Vendor].[VendorFPDShistoryPlatformSubCustomerDirectDiscretization]    Script Date: 10/5/2017 9:47:32 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+Alter VIEW [Vendor].[VendorHistoryNaicsPlatformSubCustomer]
+AS
+
+SELECT C.Fiscal_year
+, ISNULL(Agency.Customer, Agency.AGENCYIDText) as Customer
+, Agency.SubCustomer
+,coalesce(proj.PlatformPortfolio, Agency.PlatformPortfolio, cpc.PlatformPortfolio, psc.platformPortfolio) as platformPortfolio
+,	 CASE
+		WHEN Parent.ParentID is not null and isnull(Parent.UnknownCompany,0)=0 
+		THEN Parent.EntityID 
+		WHEN c.parentdunsnumber is not null and isnull(ParentSquared.UnknownCompany,0)=0 
+		THEN ParentDuns.EntityID
+		WHEN c.dunsnumber is not null and isnull(Parent.UnknownCompany,0)=0 
+		THEN DUNS.EntityID
+		ELSE coalesce(vn.EntityID,Duns.entityID)
+		 end  as EntityID
+, isnull(parent.parentid,C.dunsnumber) AS AllContractor 
+, parent.parentid
+, Parent.UnknownCompany
+, Parent.Top100Federal
+, Parent.IsSiliconValley
+, coalesce(PCN.CSISName, parent.parentid + '^',C.dunsnumber + '^')  as ContractorDisplayName
+, Parent.jointventure
+, C.obligatedAmount
+, C.numberOfActions
+, n.NAICS_Code
+, n.Industry_TEXT
+--Grouping Sub-Query
+, IIf(C.contractingofficerbusinesssizedetermination='S' 
+   And Not (parent.largegreaterthan3B=1 Or parent.Largegreaterthan3B=1)
+   ,1
+   ,0) AS Small
+, iif(parent.parentid is null or
+		parent.firstyear>c.fiscal_year or
+		parent.mergeryear<=c.fiscal_year,1,0) as WarningFlag
+FROM Contract.FPDS as C
+			left outer join FPDSTypeTable.AgencyID AS Agency
+			ON C.contractingofficeagencyid = Agency.AgencyID 
+		LEFT JOIN Contractor.DunsnumberToParentContractorHistory as DtPCH
+			ON (C.DUNSNumber=DtPCH.DUNSNUMBER) 
+			AND (C.fiscal_year=DtPCH.FiscalYear) 
+		LEFT JOIN Contractor.ParentContractor as Parent
+			ON DtPCH.ParentID=Parent.ParentID	
+		LEFT JOIN FPDSTypeTable.ProductOrServiceCode AS PSC 
+			ON PSC.ProductOrServiceCode = C.productorservicecode
+		left outer join FPDSTypeTable.ClaimantProgramCode cpc
+			on c.claimantprogramcode=cpc.claimantprogramcode
+--Block of CSISIDjoins
+              left join contract.csistransactionid as CTID
+                     on ctid.CSIStransactionID=c.CSIStransactionID
+              left join contract.CSISidvmodificationID as idvmod
+                     on idvmod.CSISidvmodificationID=ctid.CSISidvmodificationID
+              left join contract.CSISidvpiidID as idv
+                     on idv.CSISidvpiidID=idvmod.CSISidvpiidID
+              left join contract.CSIScontractID as cid
+                     on cid.CSIScontractID=ctid.CSIScontractID
+--Block of Contract Label and ProjectID 
+              left join Contract.ContractLabelID label
+                     on coalesce(ctid.ContractLabelID,cid.COntractlabelid,idv.ContractLabelID) = label.ContractLabelID
+              LEFT JOIN Project.SystemEquipmentCodetoProjectIDhistory as SYS
+                     ON SYS.systemequipmentcode=C.systemequipmentcode
+                     and SYS.StartFiscalYear <= c.fiscal_year
+                     and isnull(SYS.EndFiscalYear,9999) >= c.fiscal_year
+              left join project.projectID Proj
+                     on proj.projectid=isnull(sys.projectid,label.PrimaryProjectID)
+	LEFT OUTER JOIN Contract.ContractDiscretization AS CD
+		ON CD.CSIScontractID = CTID.CSIScontractID
+--End Grouping Subquery
+	LEFT JOIN Contractor.ParentContractorNameHistory as PCN
+		ON Parent.ParentID = PCN.ParentID
+		AND C.Fiscal_Year = PCN.FiscalYear
+	left outer join FPDSTypeTable.NAICScode n
+		on c.principalnaicscode=n.NAICS_Code
+	--Vendor specific things
+		left outer join contract.UnlabeledDunsnumberUniqueTransactionIDentityIDhistory u
+		on u.unique_transaction_id=c.unique_transaction_id
+	
+			LEFT OUTER JOIN Contractor.Dunsnumber as DUNS
+			ON DUNS.Dunsnumber=DtPCH.Dunsnumber
+					left outer join Vendor.VendorName vn
+		on vn.VendorName=u.StandardizedVendorName
+	
+			LEFT OUTER JOIN Contractor.DunsnumbertoParentContractorHistory as ParentDtPCH
+			ON ParentDtPCH.FiscalYear=C.fiscal_year  AND  ParentDtPCH.DUNSnumber=C.parentdunsnumber
+		LEFT OUTER JOIN Contractor.Dunsnumber as ParentDUNS
+			ON ParentDUNS.Dunsnumber=ParentDtPCH.Dunsnumber
+		LEFT OUTER JOIN Contractor.ParentContractor as PARENTsquared
+			ON PARENTsquared.ParentID= ParentDtPCH.ParentID ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+GO
+
+
