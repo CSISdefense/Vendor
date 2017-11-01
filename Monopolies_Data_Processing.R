bde1ef7a8ce46b689cad7353cbec6457ff9c6312
Greg Sanders <GSanders@csis.org>
2017-11-01 17:37:10 -0400

    Fine tuning.

--- Monopolies_Data_Processing.R
+++ Monopolies_Data_Processing.R
@@ -4,7 +4,7 @@
 rm(list = ls())
 library(tidyverse)
 library(csis360)
-
+ddply cannot
   
 # Path<-"C:\\Users\\gsand_000.ALPHONSE\\Documents\\Development\\R-scripts-and-data\\"
 Path<-"K:\\2007-01 PROFESSIONAL SERVICES\\R scripts and data\\"
@@ -24,7 +24,7 @@ defense_naics_vendor <- read.table(file,
                            quote="\"",#Necessary because there are some 's in the names.
                            sep = "\t")
 
-defense_naics_vendor<-apply_lookups(Path,defense_naics_vendor)
+
 
 #Import Defense Vendor list.
 file<-unz("Data\\Defense_Vendor_EntityIDhistory.zip",
@@ -36,7 +36,10 @@ defense_vendor <- read.table(file,
                                    quote="\"",#Necessary because there are some 's in the names.
                                    sep = "\t")
 
+
+
 defense_vendor<-apply_lookups(Path,defense_vendor)
+defense_naics_vendor<-apply_lookups(Path,defense_naics_vendor)
 
 
 defense_vendor<-csis360::read_and_join(defense_vendor,
@@ -49,19 +52,60 @@ defense_vendor<-csis360::read_and_join(defense_vendor,
                                        # NA.check.columns="Fair.Competed"
 )
 
-unique(defense_vendor$Abbreviation)
+defense_naics_vendor<-csis360::read_and_join(defense_naics_vendor,
+  "Lookup_ParentID.csv",
+  path="https://raw.githubusercontent.com/CSISdefense/Lookup-Tables/master/",
+  by="ParentID",
+  directory="vendor//",
+  new_var_checked=FALSE,
+  add_var="Abbreviation"
+  # NA.check.columns="Fair.Competed"
+)
 
+defense_vendor<- defense_vendor %>%
+  group_by(Fiscal.Year) %>%
+  transform(percentage = sum(Action.Obligation))
 
 
 defense_vendor<-ddply(defense_vendor,
                       .(Fiscal.Year),
-                      transform, 
+                      transform,
                       pos = rank(-Action.Obligation,
                                  ties.method ="min"),
                       pct = Action.Obligation / sum(Action.Obligation))
 
+defense_naics_vendor<- defense_naics_vendor %>%
+  group_by(Fiscal.Year,NAICS_Code) %>%
+  transform(pct = Action.Obligation / sum(Action.Obligation))
+  # dplyr::ddply(defense_naics_vendor,
+  # .(Fiscal.Year,NAICS_Code),
+  # transform, 
+  # pos = rank(-Action.Obligation,
+  #   ties.method ="min"),
+  # pct = Action.Obligation / sum(Action.Obligation))
 
-save(defense_naics_vendor,defense_vendor,file="data//defense_naics_vendor.Rdata")
+
+annual_summary<-ddply(defense_vendor,
+  .(Fiscal.Year),
+  summarize, 
+  Action.Obligation = sum(Action.Obligation),
+  vendor_count=length(Fiscal.Year),
+  hh_index=sum((pct*100)^2)
+)
+
+annual_naics_summary<-ddply(defense_naics_vendor,
+  .(Fiscal.Year,NAICS_Code,Industry_Text),
+  summarize, 
+  Action.Obligation = sum(Action.Obligation),
+  vendor_count=length(Fiscal.Year),
+  hh_index=sum((pct*100)^2)
+)
+
+
+save(defense_naics_vendor,
+  defense_vendor,
+  annual_summary,
+  file="data//defense_naics_vendor.Rdata")
 
 
 
