f29602467dd17fba0448b332e5d08d0d0cd84247
Gregory Sanders <grysar@zort.net>
2018-04-17 10:52:14 -0400

    SQL files after chat

---
+++ VendorSQL/Vendor.sp_EntityCountHistoryCustomer.sql
@@ -0,0 +1,98 @@
+/****** Object:  StoredProcedure [Vendor].[sp_EntityCountHistoryCustomer]    Script Date: 4/17/2018 10:39:22 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+ALTER PROCEDURE [Vendor].[sp_EntityCountHistoryCustomer]
+@Customer VARCHAR(255)
+
+AS
+
+	IF (@Customer is not null) --Begin sub path where only services only one Customer will be returned
+	BEGIN
+		--Copy the start of your query here
+			SELECT [fiscal_year]
+		  ,Customer
+		  ,ent.EntitySizeCode      
+		  ,esc.EntitySizeText
+		  ,EntityCategory
+		  ,AnyEntityUSplaceOfPerformance
+		  ,AnyEntityForeignPlaceOfPerformance
+		  ,IsEntityAbove2016constantOneMillionThreshold
+		  ,IsEntityAbove1990constantReportingThreshold
+		  ,IsEntityAbove2016constantReportingThreshold
+		  ,count(distinct [EntityID]) [EntityCount]
+		  ,count(distinct [AllContractor]) as [AllContractorCount]
+		  ,sum([numberOfActions]) as SumOfNumberOfActions
+		  ,sum([ObligatedAmount]) as [SumOfObligatedAmount]
+	  FROM [Vendor].EntitySizeHistoryBucketPlatformSubCustomer ent
+	  left outer join Vendor.EntitySizeCode esc
+	  on esc.EntitySizeCode=ent.entitysizecode
+	  --Here's the where clause for @ServicesOnly=1 and Customer is not null
+		WHERE ent.Customer=@Customer
+		--Copy the end of your query here
+	  group by 	  [fiscal_year]
+	  			,Customer
+	  ,ent.EntitySizeCode
+	  ,esc.EntitySizeText
+	  ,EntityCategory
+	  ,AnyEntityUSplaceOfPerformance
+	  ,AnyEntityForeignPlaceOfPerformance
+	  		  ,IsEntityAbove2016constantOneMillionThreshold
+	  ,IsEntityAbove1990constantReportingThreshold
+		  ,IsEntityAbove2016constantReportingThreshold
+		--End of your query
+		END
+	ELSE --Begin sub path where only services but all Customers will be returned
+		BEGIN
+		--Copy the start of your query here
+			SELECT [fiscal_year]
+			,ent.Customer
+			,ent.EntitySizeCode      
+			,esc.EntitySizeText
+			,EntityCategory
+		  ,AnyEntityUSplaceOfPerformance
+		  ,AnyEntityForeignPlaceOfPerformance
+		  		  ,IsEntityAbove2016constantOneMillionThreshold
+		  ,IsEntityAbove1990constantReportingThreshold
+		  ,IsEntityAbove2016constantReportingThreshold
+		  ,count(distinct [EntityID]) [EntityCount]
+		  ,count(distinct [AllContractor]) as [AllContractorCount]
+		  ,sum([numberOfActions]) as SumOfNumberOfActions
+		  ,sum([ObligatedAmount]) as [SumOfObligatedAmount]
+	  FROM [Vendor].EntitySizeHistoryBucketPlatformSubCustomer ent
+	  left outer join Vendor.EntitySizeCode esc
+	  on esc.EntitySizeCode=ent.entitysizecode
+	  group by 	  [fiscal_year]
+	  			,Customer
+	  ,ent.EntitySizeCode
+	  ,esc.EntitySizeText
+	  ,EntityCategory
+	  ,AnyEntityUSplaceOfPerformance
+	  		  ,AnyEntityForeignPlaceOfPerformance
+	  		  ,IsEntityAbove2016constantOneMillionThreshold
+	  ,IsEntityAbove1990constantReportingThreshold
+		  ,IsEntityAbove2016constantReportingThreshold
+		--End of your query
+		END
+
+	
+
+
+
+
+
+
+
+
+
+
+GO
+
+
