fcc192ea5a040cf77f443c359231de468c9123e8
Unknown <MRoth@csis.org>
2018-04-19 12:12:21 -0400

    Update 419

--- VendorSQL/OutputToFile.sql
+++ VendorSQL/OutputToFile.sql
@@ -1,5 +1,9 @@
-USE [DIIG]
+USE CSIS360
 GO
+
+
+--For instructions see https://github.com/CSISdefense/DIIGsql/blob/master/Doc/Output_Large_Dataset.md
+
 
 SET ANSI_WARNINGS OFF;
 SET NOCOUNT ON;
@@ -13,4 +17,57 @@ EXEC	@return_value = [Vendor].[SP_EntityIDhistoryNAICS]
 		
 --SELECT	'Return Value' = @return_value
 
-GO
+
+
+SET ANSI_WARNINGS OFF;
+SET NOCOUNT ON;
+
+DECLARE	@return_value int
+
+--EXEC	@return_value = [Vendor].[SP_EntityIDhistory]
+EXEC	@return_value = Vendor.sp_EntityCountHistoryPlatformSubCustomer
+--EXEC	@return_value = Contract.[SP_ContractBudgetDecisionTree]
+		@Customer = 'Defense'
+		
+--SELECT	'Return Value' = @return_value
+
+
+SET ANSI_WARNINGS OFF;
+SET NOCOUNT ON;
+
+DECLARE	@return_value int
+
+--EXEC	@return_value = [Vendor].[SP_EntityIDhistory]
+EXEC	@return_value = Vendor.sp_EntityCountHistorySubCustomer
+--EXEC	@return_value = Contract.[SP_ContractBudgetDecisionTree]
+		@Customer = 'Defense'
+		
+--SELECT	'Return Value' = @return_value
+
+
+SET ANSI_WARNINGS OFF;
+SET NOCOUNT ON;
+
+DECLARE	@return_value int
+
+--EXEC	@return_value = [Vendor].[SP_EntityIDhistory]
+EXEC	@return_value = Vendor.sp_EntityCountHistoryPlatformCustomer
+--EXEC	@return_value = Contract.[SP_ContractBudgetDecisionTree]
+		@Customer = 'Defense'
+		
+--SELECT	'Return Value' = @return_value
+
+
+SET ANSI_WARNINGS OFF;
+SET NOCOUNT ON;
+
+DECLARE	@return_value int
+
+--EXEC	@return_value = [Vendor].[SP_EntityIDhistory]
+EXEC	@return_value = Vendor.sp_EntityCountHistoryCustomer
+--EXEC	@return_value = Contract.[SP_ContractBudgetDecisionTree]
+		@Customer = 'Defense'
+		
+--SELECT	'Return Value' = @return_value
+
+
