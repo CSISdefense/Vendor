3f08f698091bbad4b6b5b64f78e539fa3b2a0acb
Gregory Sanders <gsanders@csis.org>
2019-02-27 08:22:13 -0500

    Output for chartmaker.

--- VendorSQL/OutputToFile.sql
+++ VendorSQL/OutputToFile.sql
@@ -9,6 +9,18 @@ GO
 SET ANSI_WARNINGS OFF;
 SET NOCOUNT ON;
 
+
+DECLARE	@return_value int
+
+--EXEC	@return_value = [Vendor].[SP_EntityIDhistory]
+EXEC	@return_value = Summary.SP_CompetitionVendorSizeHistoryBucketPlatformSubCustomer
+--EXEC	@return_value = Contract.[SP_ContractBudgetDecisionTree]
+		@Customer = 'Defense',
+		@SubCustomer = NULL,
+		@PlatformPortfolio =NULL
+
+
+
 DECLARE	@return_value int
 
 --EXEC	@return_value = [Vendor].[SP_EntityIDhistory]
