a001209bfc0f837e2f8739c190eb0ae5fdaede00
Justin Graham <justin.dav.graham@gmail.com>
2019-10-10 17:26:39 -0400

    Rda files and updated code for competition run

--- Scripts/VendorSQL/OutputToFile.sql
+++ Scripts/VendorSQL/OutputToFile.sql
@@ -117,8 +117,9 @@ SET NOCOUNT ON;
 DECLARE	@return_value int
 
 --EXEC	@return_value = [Vendor].[SP_EntityIDhistory]
-EXEC	@return_value = [Vendor].[SP_EntityIDhistoryNAICS]
+--EXEC	@return_value = [Vendor].[SP_EntityIDhistoryNAICS]
 --EXEC	@return_value = Contract.[SP_ContractBudgetDecisionTree]
+EXEC	@return_value = [Contract].[SP_ContractCompetitionVehicleCustomer]
 		@Customer = 'Defense'
 		
 --SELECT	'Return Value' = @return_value
