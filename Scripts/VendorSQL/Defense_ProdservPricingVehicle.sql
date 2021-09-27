55d5bcd61c49e7869d4c6f34fdbd81af5608f2ea
Greg Sanders <gregory.sanders@gmail.com>
2021-09-27 08:25:24 -0400

    Software Research

---
+++ Scripts/VendorSQL/Defense_ProdservPricingVehicle.sql
@@ -0,0 +1,41 @@
+
+
+SET ANSI_WARNINGS OFF;
+SET NOCOUNT ON;
+
+select productorservicecode
+,ServicesCategory
+,IsService
+,Simple
+,ProductOrServiceArea
+,ProductServiceOrRnDarea
+,ProductOrServiceCode
+,ProductOrServiceCodeText
+,Contractingcustomer
+,contractingofficeagencyid
+,ContractingAgencyText
+,fiscal_year
+ --Pricing
+	     ,[TypeOfContractPricing5Category]
+	  	  ,PricingFee
+		  , PricingUCA
+		  ,VehicleClassification
+,sum(obligatedamount) as obligatedamount
+from contract.[FPDSclassification]
+where Contractingcustomer='Defense'-- and  ProductOrServiceArea in ('Electronics & Communications'  , 'ICT')
+group by  productorservicecode
+,ServicesCategory
+,IsService
+,Simple
+,ProductOrServiceArea
+,ProductServiceOrRnDarea
+,ProductOrServiceCode
+,ProductOrServiceCodeText
+,Contractingcustomer
+,contractingofficeagencyid
+,ContractingAgencyText
+,fiscal_year
+	     ,[TypeOfContractPricing5Category]
+	  	  ,PricingFee
+		  , PricingUCA
+		  ,VehicleClassification
