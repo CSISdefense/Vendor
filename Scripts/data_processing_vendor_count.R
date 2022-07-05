691d65962917e7145e48fbaeca201b6f20150628
Gregory Sanders <gregory.sanders@gmail.com>
2022-07-05 17:51:30 -0400

    Entity Count

--- Scripts/data_processing_vendor_count.R
+++ Scripts/data_processing_vendor_count.R
@@ -25,28 +25,28 @@ library(csis360)
 
   
   
-  standardize_count<-function(c){
-    c<-standardize_variable_names(c)
-    c%<>%
-      # select(-Customer) %>%
-      mutate(
-        NumberOfActions = text_to_number(NumberOfActions),
-        Action_Obligation = text_to_number(Action_Obligation))%>%
-      mutate(
-        EntitySizeText.detail = EntitySizeText,
-        EntitySizeText = fct_recode(
-          EntitySizeText,
-          Small = "Always Small Vendor",
-          Small = "Sometimes Small Vendor",
-          Medium = "Medium Vendor",
-          "Large+" = "Big Five",
-          "Large+" = "Large Vendor",
-          "Large+" = "Large: Big 5 JV"))%>%
-      filter(Fiscal_Year >= 2000)
-    c
-  }
-  
-  key<-c("Fiscal_Year", "EntitySizeText", "EntitySizeText.detail",
+  # standardize_count<-function(c){
+  #   c<-standardize_variable_names(c)
+  #   c%<>%
+  #     # select(-Customer) %>%
+  #     mutate(
+  #       NumberOfActions = text_to_number(NumberOfActions),
+  #       Action_Obligation = text_to_number(Action_Obligation))%>%
+  #     mutate(
+  #       EntitySizeText.detail = EntitySizeText,
+  #       EntitySizeText = fct_recode(
+  #         EntitySizeText,
+  #         Small = "Always Small Vendor",
+  #         Small = "Sometimes Small Vendor",
+  #         Medium = "Medium Vendor",
+  #         "Large+" = "Big Five",
+  #         "Large+" = "Large Vendor",
+  #         "Large+" = "Large: Big 5 JV"))%>%
+  #     filter(Fiscal_Year >= 2000)
+  #   c
+  # }
+
+  key<-c("Fiscal_Year", "EntitySizeCode",
          "EntityCategory",
          "AnyEntityUSplaceOfPerformance",
          "AnyEntityForeignPlaceOfPerformance",
@@ -55,132 +55,24 @@ library(csis360)
          "IsEntityAbove2016constantReportingThreshold"
          )
   
-  
-  platform_sub<-standardize_count(platform_sub)
+  platform_sub<-apply_standard_lookups(platform_sub)
   check_key(platform_sub,c("SubCustomer", "PlatformPortfolio",key))
-  platform_only<-standardize_count(platform_only %>% filter(Customer=="Defense"))
+  platform_only<-apply_standard_lookups(platform_only)
+  # platform_only<-standardize_count(platform_only %>% filter(Customer=="Defense"))
   check_key(platform_only,c("PlatformPortfolio","IsEntityAbove2018constant10ThousandThreshold",key))
     # write.csv(all_duplicate(platform_only,c("PlatformPortfolio","IsEntityAbove2018constant10ThousandThreshold",key)),"dupe.csv")
-  
-  platformUAS_only<-standardize_count(platformUAS_only)
+  platformUAS_only<-apply_standard_lookups(platformUAS_only)
   check_key(platformUAS_only,c("PlatformPortfolioRemote","IsEntityAbove2018constant10ThousandThreshold",key))
-  
-  sub_only<-standardize_count(sub_only)
+  sub_only<-apply_standard_lookups(sub_only)
   check_key(sub_only,c("SubCustomer",key))
   
-  top_level<-standardize_count(top_level)
-  check_key(top_level,c(key,"Customer"))
+  top_level<-apply_standard_lookups(top_level)
+  check_key(top_level,c(key,"Customer","IsEntityAbove2018constant10ThousandThreshold"))
   
-  # # remove unused variables
-  # platform_sub  %>%
-  #   group_by(
-  #     Fiscal_Year, SubCustomer, PlatformPortfolio, EntitySizeText, EntitySizeText.detail,
-  #     EntityCategory,
-  #     AnyEntityUSplaceOfPerformance,
-  #     AnyEntityForeignPlaceOfPerformance,
-  #     IsEntityAbove2016constantOneMillionThreshold,
-  #     IsEntityAbove1990constantReportingThreshold,
-  #     IsEntityAbove2016constantReportingThreshold
-  #     ) %>%
-  #   summarize(
-  #     EntityCount = sum(EntityCount), 
-  #     AllContractorCount = sum(AllContractorCount),
-  #     NumberOfActions = sum(NumberOfActions),
-  #     Action_Obligation = sum(Action_Obligation)) 
-  
-  
-  
-  platform_only %<>%
-    select(-Customer) %>%
-    rename(EntityCount = EntityCount) %>%
-    mutate(
-      NumberOfActions = as.character(NumberOfActions),
-      Action_Obligation = as.character(Action_Obligation)) %>%
-    mutate(
-      NumberOfActions = as.integer(
-        ifelse(is.na(NumberOfActions), 0, NumberOfActions)),
-      Action_Obligation = as.numeric(
-        ifelse(is.na(Action_Obligation), 0, Action_Obligation))) %>%
-    mutate(
-      EntitySizeText.detail = EntitySizeText,
-      EntitySizeText = fct_recode(
-        EntitySizeText,
-        Small = "Always Small Vendor",
-        Small = "Sometimes Small Vendor",
-        Medium = "Medium Vendor",
-        "Large+" = "Big Five",
-        "Large+" = "Large Vendor",
-        "Large+" = "Large: Big 5 JV")) %>%
-    group_by(
-      Fiscal_Year, PlatformPortfolio, EntitySizeText, EntitySizeText.detail,
-      EntityCategory,
-      AnyEntityUSplaceOfPerformance,
-      AnyEntityForeignPlaceOfPerformance,
-      IsEntityAbove2016constantOneMillionThreshold,
-      IsEntityAbove1990constantReportingThreshold,
-      IsEntityAbove2016constantReportingThreshold) %>%
-    summarize(
-      EntityCount = sum(EntityCount), 
-      AllContractorCount = sum(AllContractorCount),
-      NumberOfActions = sum(NumberOfActions),
-      Action_Obligation = sum(Action_Obligation)) %>%
-    filter(Fiscal_Year >= 2000)
-                             
-    
-  names(top_level)[1] <- "Fiscal_Year"
-  
-  top_level %<>%
-    filter(Customer == "Defense") %>%
-    rename(EntityCount = EntityCount) %>%
-    select(-Customer) %>%
-    mutate(
-      NumberOfActions = as.character(NumberOfActions), 
-      Action_Obligation = as.character(Action_Obligation)) %>%
-    mutate(
-      NumberOfActions = as.integer(
-        ifelse(is.na(NumberOfActions), 0, NumberOfActions)),
-      Action_Obligation = as.numeric(
-        ifelse(is.na(Action_Obligation), 0, Action_Obligation))) %>%
-    mutate(
-      EntitySizeText.detail = EntitySizeText,
-      EntitySizeText = fct_recode(
-        EntitySizeText,
-        Small = "Always Small Vendor",
-        Small = "Sometimes Small Vendor",
-        Medium = "Medium Vendor",
-        "Large+" = "Big Five",
-        "Large+" = "Large Vendor",
-        "Large+" =  "Large: Big 5 JV")) %>%
-    group_by(
-      Fiscal_Year, EntitySizeText, EntitySizeText.detail,
-      EntityCategory,
-      AnyEntityUSplaceOfPerformance,
-      AnyEntityForeignPlaceOfPerformance,
-      IsEntityAbove2016constantOneMillionThreshold,
-      IsEntityAbove1990constantReportingThreshold,
-      IsEntityAbove2016constantReportingThreshold) %>%
-    summarize(
-      EntityCount = sum(EntityCount), 
-      AllContractorCount = sum(AllContractorCount),
-      NumberOfActions = sum(NumberOfActions),
-      Action_Obligation = sum(Action_Obligation)) %>%
-    filter(Fiscal_Year >= 2000)
-  
-  
-  
-  
-  
-sub_only <- deflate(sub_only,money_var = "Action_Obligation")
-platform_only <- deflate(platform_only,money_var = "Action_Obligation")
-platform_sub <- deflate(platform_sub,money_var = "Action_Obligation")
-platformUAS_only<- deflate(platformUAS_only,money_var = "Action_Obligation")
-top_level <- deflate(top_level,money_var = "Action_Obligation")
-
-
-
 
 prepare_vendor<-function(data)
   {
+  data <- data %>%filter(Fiscal_Year >= 2000)
   #Add this to replace NAs function
   if("SubCustomer" %in% colnames(data)){
     if(!is.factor(data$SubCustomer))
@@ -262,6 +154,13 @@ write_csv(platform_sub, file.path("analysis","FPDS_vendor_count","platform_sub.c
 write_csv(platformUAS_only, file.path("analysis","FPDS_vendor_count","platformUAS_only.csv"))
 write_csv(top_level, file.path("analysis","FPDS_vendor_count","top_level.csv"))
 
-  # write output to CleanedVendorSize.csv
+colnames(platformUAS_only)[colnames(platformUAS_only)=="EntitySizeSum"]<-"EntitySizeText.sum"
+uav_lc<-csis360::prepare_labels_and_colors(platformUAS_only)
+uav_ck<-csis360::get_column_key(platformUAS_only)
+
+# write output to CleanedVendorSize.csv
+save(platformUAS_only, uav_lc, uav_ck, 
+     file=file.path("data","clean","uav_vendor_count.Rda"))  #Shiny Apps//FPDS vendor count//
+
   save(platform_only,sub_only,platform_sub,top_level,platformUAS_only, column_key, labels_and_colors, 
        file=file.path("analysis","FPDS_vendor_count","vendor_count.Rda"))  #Shiny Apps//FPDS vendor count//
