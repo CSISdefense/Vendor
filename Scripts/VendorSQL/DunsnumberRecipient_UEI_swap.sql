b6de5e63492b2ea4da476b0ddc6eb0f8d6606cde
Gregory Sanders <GSanders@csis.org>
2023-08-08 08:04:40 -0400

    Starting duns/uei updates.

--- Scripts/VendorSQL/DunsnumberRecipient_UEI_swap.sql
+++ Scripts/VendorSQL/DunsnumberRecipient_UEI_swap.sql
@@ -5,9 +5,16 @@ where len(dunsnumber)=12 and len(recipient_uei) in (8,9)
 
 --Roughly 1 million rows an hour (2.2m at 1h57m)
 update f
-set dunsnumbertemp=dunsnumber
+set UEItemp=dunsnumber
 from contract.fpds f
 where len(dunsnumber)=12 and len(recipient_uei) in (8,9)
+
+
+--1h08m
+select count(*)
+from contract.fpds f
+where len(dunsnumber)=12 and len(recipient_uei) in (8,9) and  UEItemp is null
+
 
 --***********Dunsnumber, not present in new downloads*************
 -- 'Insert new dunsnumber into [Contractor].[dunsnumber]'
@@ -37,41 +44,32 @@ WHERE ErrorTable.recipient_uei is not null and PKTable.dunsnumber is null and
  len(errortable.dunsnumber)=12 and len(errortable.recipient_uei) in (8,9)
 GROUP BY ErrorTable.recipient_uei, errortable.fiscal_year
 
+--Stopped here from home.
 
-	-- Insert new [recipient_uei] into Vendor.recipient_uei
+-- Insert new [parent_recipient_uei] into Vendor.recipient_uei
 INSERT INTO Vendor.recipient_uei
 (recipient_uei)
-SELECT fk.[recipient_uei]
-FROM ErrorLogging.FPDSstage2 as fk
-LEFT OUTER JOIN Vendor.recipient_uei as pk
-On pk.recipient_uei=fk.[recipient_uei]
-WHERE pk.recipient_uei is NULL
-GROUP BY fk.[recipient_uei]
-
-	-- Insert new [parent_recipient_uei] into Vendor.recipient_uei
-INSERT INTO Vendor.recipient_uei
-(recipient_uei)
-SELECT fk.dunsnumbertemp
+SELECT fk.UEItemp
 FROM contract.fpds as fk
 LEFT OUTER JOIN Vendor.recipient_uei as pk
-On pk.recipient_uei=fk.dunsnumbertemp
+On pk.recipient_uei=fk.UEItemp
 WHERE pk.recipient_uei is NULL
-GROUP BY fk.dunsnumbertemp
+GROUP BY fk.UEItemp
 
 	-- Insert new [recipient_uei] into Vendor.Recipient_UEIhistory
 INSERT INTO Vendor.Recipient_UEIhistory
-(fiscal_year,dunsnumbertemp)
+(fiscal_year,UEItemp)
 SELECT fk.fiscal_year,fk.recipient_uei
 FROM contract.fpds as fk
 LEFT OUTER JOIN Vendor.Recipient_UEIhistory as pk
 On pk.recipient_uei=fk.recipient_uei and
 		pk.fiscal_year=fk.fiscal_year 
 WHERE pk.recipient_uei is NULL 
-GROUP BY fk.dunsnumbertemp,fk.fiscal_year
+GROUP BY fk.UEItemp,fk.fiscal_year
 
 update f
 set dunsnumber=recipient_uei
-	,recipient_uei=dunsnumbertemp
+	,recipient_uei=UEItemp
 from contract.fpds f
 where len(dunsnumber)=12 and len(recipient_uei) in (8,9)
-and dunsnumbertemp is not null
+and UEItemp is not null
