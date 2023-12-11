482fb4e0f0f123e24dee429693791c830ccdf51c
Gregory Sanders <GSanders@csis.org>
2023-12-11 16:59:31 -0500

    ZIpping the largest csvs.

--- Scripts/OSC/Loan_Data_Processing.R
+++ Scripts/OSC/Loan_Data_Processing.R
@@ -27,7 +27,38 @@ con <- dbConnect(odbc(),
 
 
 loan<-dbReadTable(con,  name = SQL('"Assistance"."OSCloanDataSet"'))
+loan$cfda_num<-round(text_to_number(loan$cfda_number),3)
 save(loan,file="data/semi_clean/OSC/FAADCloanDataSet.rda")
+
+#Over 13 million and no way to identify critical tech, seperating 
+loanPPP<-loan %>% filter(cfda_num==59.073)
+save(loanPPP,file="data/semi_clean/OSC/PPPloanDataSet.rda")
+
+#Around 6 million, also no clear way to identify critical tech
+loanDisaster <-loan  %>% filter(cfda_num %in% c(59.008,59.063))
+save(loanDisaster,file="data/semi_clean/OSC/SBAdisasterLoanDataSet.rda")
+
+#These are the loans we have paths to identify critical technologies
+
+loanSelected<- loan %>% filter(cfda_num %in% c(31.007,59.011,59.012,59.016,59.041,59.054,81.126))
+save(loanSelected,file="data/semi_clean/OSC/SelectedLoanDataSet.rda")
+
+loanOther <- loan %>% filter(!cfda_num %in% c(31.007,59.011,59.012,59.016,59.041,59.054,81.126,
+                                     59.073,
+                                     59.008,59.063))
+save(loanOther,file="data/semi_clean/OSC/OtherLoanDataSet.rda")
+
+
+
+loan$cdfa_title
 load(file="data/semi_clean/OSC/FAADCloanDataSet.rda")
-loan<-loan %>% filter(assistance_type_code!="09")
+
+
+
+
+
+
+test<-loan %>% group_by(cfda_title,cfda_num,assistance_type_code) %>% summarise(n=length(assistance_type_code)) %>% arrange(cfda_num)
+
+
 
