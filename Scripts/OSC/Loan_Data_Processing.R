d1bcc6712160f723432c77508d03cbc91a3b6751
Gregory Sanders <GSanders@csis.org>
2023-12-11 18:15:19 -0500

    Working on focused tagging.

--- Scripts/OSC/Loan_Data_Processing.R
+++ Scripts/OSC/Loan_Data_Processing.R
@@ -27,7 +27,16 @@ con <- dbConnect(odbc(),
 
 
 loan<-dbReadTable(con,  name = SQL('"Assistance"."OSCloanDataSet"'))
-loan$cfda_num<-round(text_to_number(loan$cfda_number),3)
+
+
+
+cfda_summary<-loan %>% group_by(cfda_title,cfda_num,assistance_type_code) %>% 
+  summarise(n=length(assistance_type_code),
+            total_outlayed_amount_for_overall_award=sum(total_outlayed_amount_for_overall_award,na.rm=TRUE),
+            face_value_of_loan=sum(face_value_of_loan,na.rm=TRUE))  %>%
+  arrange(cfda_num)
+write.csv(cfda_summary,"data/semi_clean/OSC/cfda_summary.csv",row.names = FALSE)
+
 save(loan,file="data/semi_clean/OSC/FAADCloanDataSet.rda")
 
 #Over 13 million and no way to identify critical tech, seperating 
@@ -48,19 +57,11 @@ loanOther <- loan %>% filter(!cfda_num %in% c(31.007,59.011,59.012,59.016,59.041
 save(loanOther,file="data/semi_clean/OSC/OtherLoanDataSet.rda")
 
 
-
-loan$cdfa_title
-load(file="data/semi_clean/OSC/FAADCloanDataSet.rda")
+load(file="data/semi_clean/OSC/SelectedLoanDataSet.rda")
 
 
 
 
-
-
-test<-loan %>% group_by(cfda_title,cfda_num,assistance_type_code) %>% 
-  summarise(n=length(assistance_type_code),
-            total_outlayed_amount_for_overall_award=sum(total_outlayed_amount_for_overall_award,na.rm=TRUE))  %>%
-  arrange(cfda_num)
 
 
 
